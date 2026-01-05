1c6b9acabe586a1c80c560331aa713129151c5a1
Gxlvxao <elenaofteste@gmail.com>
2026-01-05 18:18:31 -0300

    Adiciona arquivo de importação de imóveis

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,17 @@ Route::get('/ferramentas/simulador-credito', function () {
     return view('tools.credit');
 })->name('tools.credit');
 
-Route::get('/ferramentas/mais-valias', function () {
-    return view('tools.gains');
-})->name('tools.gains');
-
 Route::get('/ferramentas/imt', function () {
     return view('tools.imt');
 })->name('tools.imt');
+
+// Rota antiga (comentei para evitar conflito com a de baixo)
+// Route::get('/ferramentas/mais-valias', function () {
+//    return view('tools.gains');
+// })->name('tools.gains');
+
+Route::get('/ferramentas/mais-valias', [ToolsController::class, 'showGainsSimulator'])->name('tools.gains');
+Route::post('/ferramentas/mais-valias/calcular', [ToolsController::class, 'calculateGains'])->name('tools.gains.calculate');
 
 Route::get('/blog', function () {
     return view('blog.index');
@@ -38,9 +42,6 @@ Route::get('/blog/novo-perfil-investidor-luxo', function () {
     return view('blog.show');
 })->name('blog.show');
 
-Route::get('/ferramentas/mais-valias', [ToolsController::class, 'showGainsSimulator'])->name('tools.gains');
-Route::post('/ferramentas/mais-valias/calcular', [ToolsController::class, 'calculateGains'])->name('tools.gains.calculate');
-
 Route::get('/blog/inteligencia-mercado-redefine-investimento', function () {
     return view('blog.show-intelligence');
 })->name('blog.show-intelligence');
@@ -49,15 +50,17 @@ Route::get('/blog/lisboa-cascais-algarve-eixos-valor', function () {
     return view('blog.show-locations');
 })->name('blog.show-locations');
 
-// --- ROTA DE CONTATO ATUALIZADA ---
 Route::get('/contato', function () {
     return view('contact');
 })->name('contact');
-// ----------------------------------
 
 Route::prefix('admin')->group(function () {
     Route::get('/', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
-    Route::post('/login', [AdminAuthController::class, 'login'])->name('admin.login.submit');
+    
+    // --- PROTEÇÃO ADICIONADA AQUI ---
+    Route::post('/login', [AdminAuthController::class, 'login'])
+        ->middleware('throttle:5,1') // Máximo 5 tentativas por 1 minuto
+        ->name('admin.login.submit');
 
     Route::middleware('auth')->group(function () {
         Route::post('/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
